d449915ff30346f47212602a80cae8e3122fce15
liliuhua <liliuhua0306@gmail.com>
2020-07-11 19:07:40 -0400

    draft

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,8 +1,30 @@
 import axios from 'axios';
+import {AsyncStorage} from "react-native";
 
-export default axios.create({
+const instance = axios.create({
     // remember change baseURL each 8 hour because we used free ngrok URL
     //we will need to restart ngrok and put the new URL into the axios baseURL
     // ngrok http 3000
-    baseURL: 'http://51ca0058f291.ngrok.io'
-})
+    baseURL: 'http://a4ce4fbb4779.ngrok.io'
+});
+
+// hi axios run this function before you ever make a request to do
+instance.interceptors.request.use(
+    //called automatically anytime that we are about to make a request.
+    async (config) =>{
+        const token = await AsyncStorage.getItem('token');
+        if (token) {
+            // we authenticate ourselves with our backend API
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+
+    //called automatically anytime that there is an error with us making request
+    (err) => {
+        return Promise.reject(err);
+    }
+)
+
+
+export default instance;
--- src/components/Map.js
+++ src/components/Map.js
@@ -4,9 +4,10 @@ import MapView, {Polyline, Circle} from "react-native-maps";
 import {Context as LocationContext} from '../context/LocationContext';
 
 const Map = () => {
-    const {state:{currentLocation}} = useContext(LocationContext);
+    const {state:{currentLocation, locations}} = useContext(LocationContext);
 
-    // console.log(state)
+    // console.log(locations);
+
     if (!currentLocation){
         // spinner
         return <ActivityIndicator size="large" style={{marginTop:200}}/>
@@ -31,6 +32,7 @@ const Map = () => {
                     radius={30}
                     strokeColor="rgba(158,158,255,1.0)"
                     fillColor="rgba(158,158,255,0.3)" />
+            <Polyline coordinates={locations.map(loc =>loc.coords)} />
         </MapView>
     )
 };
